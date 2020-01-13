6c7f7793f78c36058c08d01cb69ea9786f314b50
Luka Micheletti <michelettiluka@gmail.com>
2020-01-13 13:30:07 +0100

    Deleted unused stuff

--- installDeps.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sudo apt -y update
-sudo apt -y upgrade
-sudo apt -y install gcc
-sudo apt -y install pkg-config
-sudo apt -y install libopencv-dev
--- src/calculus.h
+++
@@ -1,6 +0,0 @@
-#include <math.h>
-
-namespace oort {
-    double sigmoid(double value);
-    double fSigmoid(double value);
-}
