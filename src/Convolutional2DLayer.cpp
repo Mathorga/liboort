b97ff525fad13ce09787e1b7d6d2c93a9021d267
Luka Micheletti <michelettiluka@gmail.com>
2020-08-23 19:59:07 +0200

    Started conv2D layer step

--- src/Convolutional2DLayer.cpp
+++ src/Convolutional2DLayer.cpp
@@ -1,7 +1,20 @@
 #include "Convolutional2DLayer.h"
 
 namespace oort {
-    void Convolutional2DLayer::step(const math::dtensor input) {}
+    void Convolutional2DLayer::step(const math::dtensor input) {
+        // Create a temporary 3D input tensor of dimensions inWidth, inHeight, channelsNum.
+        math::dtensor3d input3d;
+        math::alloc(&input3d, this->inWidth, this->inHeight, this->inChannelsNum);
+
+        // Copy general input tensor to temporary 3D tensor.
+        math::copy(input3d, input);
+
+        // Temporary out size.
+        uint32_t outWidth = (this->inWidth - this->filterWidth + this->horizontalPadding + this->horizontalStride) / this->horizontalStride;
+        uint32_t outHeight = (this->inHeight - this->filterHeight + this->verticalPadding + this->verticalStride) / this->verticalStride;
+
+        
+    }
     void Convolutional2DLayer::backprop() {}
     void Convolutional2DLayer::print() {}
 }
